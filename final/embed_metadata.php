5d5f0c59abd271c43f58461ba1b20ec049cae3f8
Richard Hong <rhong04@tufts.edu>
2024-12-07 11:39:39 -0600

    cleaning up commented out code, adding new commented code showing limitation of using imagick for writing exif data

--- final/embed_metadata.php
+++ final/embed_metadata.php
@@ -10,15 +10,13 @@
 
   $selected_tags = isset($_POST['tags']) ? $_POST['tags'] : [];
 
-  // Get the remote file URL from the session
-  $file_url = $_SESSION['image_path']; // Remote FTP URL
+  $file_url = $_SESSION['image_path'];
 
   try {
     if (!class_exists('Imagick')) {
       throw new Exception('Imagick extension is not installed or enabled.');
     }
 
-    // Download the remote image to a temporary local file
     $temp_image_path = sys_get_temp_dir() . '/' . basename($file_url);
     $image_data = file_get_contents($file_url);
     if ($image_data === false) {
@@ -26,13 +24,12 @@
     }
     file_put_contents($temp_image_path, $image_data);
 
-    // Load the image using Imagick
     $imagick = new Imagick($temp_image_path);
 
-    // Embed the selected tags into the image metadata
     $tags_string = implode(', ', $selected_tags);
-    $imagick->setImageProperty('exif:ImageDescription', $tags_string);
-    $imagick->setImageProperty('exif:Artist', $_SESSION['username']);
+    // Imagick only supports writing comments to jpegs, modifying exif data does not persist
+    // https://github.com/Imagick/imagick/issues/124
+    // https://stackoverflow.com/questions/5384962/writing-exif-data-in-php
     $imagick->setImageProperty('comment', $tags_string);
 
     // Debug: Output all properties
@@ -40,22 +37,17 @@
       error_log("Key: $key, Value: $value");
     }
 
-    // Set output format
     $imagick->setImageFormat('jpeg');
 
-    // Save to a temporary output file
     $temp_output_path = sys_get_temp_dir() . '/output_image.jpg';
     $imagick->writeImage($temp_output_path);
 
-    // Output the image to the client
     $image_type = mime_content_type($temp_output_path);
     header('Content-Type: ' . $image_type);
     header('Content-Disposition: attachment; filename="modified_image.jpg"');
     readfile($temp_output_path);
 
-    // Clean up
-      unlink($temp_output_path);
-
+    unlink($temp_output_path);
   } catch (Exception $e) {
     if (isset($imagick)) {
       $imagick->destroy();
